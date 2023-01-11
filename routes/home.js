3b7b714ff338be433066dbd8ba49949854f576d8
Sonja Rouwhorst <s.e.rouwhorst@hva.nl>
2023-01-11 23:51:59 +0100

    add sorting on home

--- routes/home.js
+++ routes/home.js
@@ -2,10 +2,28 @@ const express = require('express')
 const router = express.Router()
 const config = require('../config/config.json')
 
+function sortById (a,b) {
+    if (a.id > b.id) {
+        return 1;
+    }
+    if (a.id < b.id) {
+        return -1;
+    }
+    return 0;
+}
+
 
 router.get('/', function (req, res) {
-    res.render('home', {cardlist: req.cardlist});
+    let sortedlist = [...req.cardlist].filter(el => el.status == "active");
+    if (!req.query.sort  || req.query.sort == "alphabetical") {
+        sortedlist = sortedlist.sort(sortById);
+        sortedlist = sortedlist.filter(el => el.type == "card")
+        res.render('home', {cardlist: sortedlist});
+    } else {
+        res.render('home', {cardlist: sortedlist});
+    }
 })
+router.get('/')
 
 
 module.exports = router;
