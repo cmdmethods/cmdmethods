bf71938f4fb713144b7d368c590943e03be5d1b7
Sonja Rouwhorst <s.e.rouwhorst@hva.nl>
2023-01-29 22:02:59 +0100

    refactor

--- routes/cards.js
+++ routes/cards.js
@@ -2,23 +2,14 @@ const express = require('express')
 const router = express.Router()
 const config = require('../config/config.json')
 
-function sortById (a,b) {
-    if (a.id > b.id) { return 1 }
-    if (a.id < b.id) { return -1 }
-    return 0;
-}
 
-function sortByStrategy (a,b) {
-    if (config.categories.indexOf(a.strategy) == config.categories.indexOf(b.strategy)) {
-        if (a.type < b.type) { return 1 }
-        else if (a.type > b.type) { return -1 } 
-        else {
-            if (a.id > b.id) { return 1}
-            if (a.id < b.id) { return -1}
-        }
+function sortByTypeAndId (a,b) {
+    if (a.type < b.type) { return 1 }
+    else if (a.type > b.type) { return -1 } 
+    else {
+        if (a.id > b.id) { return 1}
+        if (a.id < b.id) { return -1}
     }
-    if (config.categories.indexOf(a.strategy) > config.categories.indexOf(b.strategy)) { return 1 } 
-    if (config.categories.indexOf(a.strategy) < config.categories.indexOf(b.strategy)) { return -1 }
     return 0;
 }
 
@@ -26,7 +17,7 @@ function sortByStrategy (a,b) {
 router.get('/:strategy', function (req, res) {
     let sortedlist = [...req.cardlist].filter(el => el.status == "active");
     sortedlist = sortedlist.filter(el => el.strategy == req.params.strategy)
-    sortedlist = sortedlist.sort(sortByStrategy);
+    sortedlist = sortedlist.sort(sortByTypeAndId);
     res.render('strategy', {strategy: req.params.strategy, cardlist: sortedlist});
 })
 
