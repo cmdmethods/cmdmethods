9e86a87cdaa5f580332a0e47912adad3e94f9b08
Sonja Rouwhorst <s.e.rouwhorst@hva.nl>
2023-01-29 22:00:26 +0100

    add strategy template

--- routes/cards.js
+++ routes/cards.js
@@ -2,10 +2,35 @@ const express = require('express')
 const router = express.Router()
 const config = require('../config/config.json')
 
+function sortById (a,b) {
+    if (a.id > b.id) { return 1 }
+    if (a.id < b.id) { return -1 }
+    return 0;
+}
 
-router.get('/', function (req, res) {
-    res.send('cards');
+function sortByStrategy (a,b) {
+    if (config.categories.indexOf(a.strategy) == config.categories.indexOf(b.strategy)) {
+        if (a.type < b.type) { return 1 }
+        else if (a.type > b.type) { return -1 } 
+        else {
+            if (a.id > b.id) { return 1}
+            if (a.id < b.id) { return -1}
+        }
+    }
+    if (config.categories.indexOf(a.strategy) > config.categories.indexOf(b.strategy)) { return 1 } 
+    if (config.categories.indexOf(a.strategy) < config.categories.indexOf(b.strategy)) { return -1 }
+    return 0;
+}
+
+
+router.get('/:strategy', function (req, res) {
+    let sortedlist = [...req.cardlist].filter(el => el.status == "active");
+    sortedlist = sortedlist.filter(el => el.strategy == req.params.strategy)
+    sortedlist = sortedlist.sort(sortByStrategy);
+    res.render('strategy', {strategy: req.params.strategy, cardlist: sortedlist});
 })
 
 
 module.exports = router;
+
+
