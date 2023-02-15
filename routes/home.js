85d07a66548df4e38bb611e7e0d449b9572cf862
Sonja Rouwhorst <s.e.rouwhorst@hva.nl>
2023-02-15 13:32:13 +0100

    bugfixjes

--- routes/home.js
+++ routes/home.js
@@ -24,12 +24,17 @@ function sortByStrategy (a,b) {
 
 
 router.get('/', function (req, res) {
+    // filter out only active cards, not archived cards
     let sortedlist = [...req.cardlist].filter(el => el.status == "active");
+    sortedlist.forEach(el => console.log(el.id))
     if (!req.query.sort  || req.query.sort == "strategy") {
+        // sort cards by strategy
         sortedlist = sortedlist.sort(sortByStrategy);
         res.render('home', {strategy: 'home', cardlist: sortedlist});
     } else {
+        // sort cards by cardid
         sortedlist = sortedlist.sort(sortById);
+        // filter out only cards, not category descriptions
         sortedlist = sortedlist.filter(el => el.type == "card")
         res.render('home', {strategy: 'home', cardlist: sortedlist});
     }
