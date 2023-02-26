ac2d34d7fbb0ad9939acd8e171f80ddba21fd7bb
Sonja Rouwhorst <s.e.rouwhorst@hva.nl>
2023-02-26 12:00:54 +0100

    add small screen menu

---
+++ public/js/menu.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+  
+    document.getElementById('menu-categories').onclick = (e) => {
+        document.getElementById('categories').classList.toggle('fly-in');
+    }
+
+};
