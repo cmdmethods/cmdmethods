32d427996fdcebdbe2c612eee8e4d6563f334c0b
Sonja Rouwhorst <s.e.rouwhorst@hva.nl>
2023-03-05 10:53:46 +0100

    add api route and refactor filter numbers

---
+++ routes/api.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const cardController = require('../controllers/cardController');
+
+router.get('/filter-numbers', cardController.getFilterNumbers);
+
+router.use( (req, res) => {
+    res.status(404).json({error: {message:"404 - endpoint not found, check the url of your request"}});
+});
+
+
+module.exports = router;
