4dda900b58c12b986aab840b72d23a9c31da6174
rijkvanzanten <rijkvanzanten@me.com>
2023-04-10 11:40:05 -0400

    Remove onload wrapper in favor of defer

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,68 +1,62 @@
-window.onload = () => {
-    document.getElementById("menu-categories").onclick = () => {
-        document.getElementById("categories").classList.toggle("fly-in");
+document.getElementById("menu-categories").onclick = () => {
+    document.getElementById("categories").classList.toggle("fly-in");
+};
+
+document.getElementById("filter-options-menu").onclick = () => {
+    document.getElementById("filter-options").classList.toggle("come-in");
+};
+
+document.getElementById("close-filter-options").onclick = () => {
+    document.getElementById("filter-options").classList.remove("come-in");
+};
+
+const filterCheckboxes = document.querySelectorAll(
+    "#filter-options input[type=checkbox]"
+);
+
+filterCheckboxes.forEach((filterCheckbox) => {
+    filterCheckbox.onchange = () => {
+        updateFilterNumbers();
     };
+});
 
-    document.getElementById("filter-options-menu").onclick = () => {
-        document.getElementById("filter-options").classList.toggle("come-in");
-    };
+function updateFilterNumbers() {
+    const form = document.querySelector("#filter-options form");
+    const formData = new FormData(form);
+    const queryString = new URLSearchParams(formData).toString();
 
-    document.getElementById("close-filter-options").onclick = () => {
-        document.getElementById("filter-options").classList.remove("come-in");
-    };
-
-    const filterCheckboxes = document.querySelectorAll(
-        "#filter-options input[type=checkbox]"
-    );
-
-    filterCheckboxes.forEach((filterCheckbox) => {
-        filterCheckbox.onchange = () => {
-            updateFilterNumbers();
-        };
-    });
-
-    function updateFilterNumbers() {
-        const form = document.querySelector("#filter-options form");
-        const formData = new FormData(form);
-        const queryString = new URLSearchParams(formData).toString();
-
-        fetch(
-            `https://cmdmethods.onrender.com/api/filter-numbers?${queryString}`
-        )
-            .then((response) => response.json())
-            .then((filterNumbers) => {
-                const list = document.querySelectorAll(".filterNumber");
-                list.forEach((el) => {
-                    if (filterNumbers[el.dataset.filter] != null) {
-                        if (filterNumbers[el.dataset.filter] === 0) {
-                            el.parentElement.previousElementSibling.disabled = true;
-                        } else {
-                            el.parentElement.previousElementSibling.disabled = false;
-                        }
-                        el.textContent = `(${
-                            filterNumbers[el.dataset.filter]
-                        })`;
+    fetch(`https://cmdmethods.onrender.com/api/filter-numbers?${queryString}`)
+        .then((response) => response.json())
+        .then((filterNumbers) => {
+            const list = document.querySelectorAll(".filterNumber");
+            list.forEach((el) => {
+                if (filterNumbers[el.dataset.filter] != null) {
+                    if (filterNumbers[el.dataset.filter] === 0) {
+                        el.parentElement.previousElementSibling.disabled = true;
+                    } else {
+                        el.parentElement.previousElementSibling.disabled = false;
                     }
-                });
-                document.querySelector(
-                    "#filter-options input[type=submit]"
-                ).value = `Show methods (${filterNumbers.total})`;
-            })
-            .catch((error) => console.log(error));
-    }
+                    el.textContent = `(${filterNumbers[el.dataset.filter]})`;
+                }
+            });
+            document.querySelector(
+                "#filter-options input[type=submit]"
+            ).value = `Show methods (${filterNumbers.total})`;
+        })
+        .catch((error) => console.log(error));
+}
 
-    updateFilterNumbers();
+updateFilterNumbers();
 
-    window.onscroll = function () {
-        const categoryLogos = document.querySelectorAll(".flipable");
-        categoryLogos.forEach((element) => {
-            const observer = new IntersectionObserver(
-                (entries) => {
-                    element.classList.toggle("flip", entries[0].isIntersecting);
-                },
-                { threshold: 1 }
-            );
-            observer.observe(element);
-        });
-    };
+window.onscroll = function () {
+    const categoryLogos = document.querySelectorAll(".flipable");
+    categoryLogos.forEach((element) => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                element.classList.toggle("flip", entries[0].isIntersecting);
+            },
+            { threshold: 1 }
+        );
+        observer.observe(element);
+    });
 };
